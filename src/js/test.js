09bc3b6c1a6660dfd8950ae3ec3daebbf42ba5ac
antonella <antonella_caroca@hotmail.com>
2018-08-19 01:05:07 -0300

    agrego tests

--- src/js/test.js
+++ src/js/test.js
@@ -1,32 +1,15 @@
-window.addTask = (mesage) => {
-    const newTaskKey = firebase.database().ref().child('chat').push().key;
-    return firebase.database().ref('chat/' + newTaskKey).set({
-        message: message
-    });
-};
+const obtenerListaMensajes = (mensajes) => {
+  
+    mensajes = {};
+    
+      if(mensajes.lenght > 0) {
+        return 'hay mensajes';
+    } else if (mensajes.length < 1) {
+        return 'no hay mensajes';
+    }
+    
+      return mensajes;
+    };
+    
+    module.exports = obtenerListaMensajes;
 
-window.getTaskList = () => {
-    return firebase.database().ref('chat').once('value');
-}
-
-window.taskProgress = (message, state) => {
-    return firebase.database().ref('chat').once("value").then(
-        (results) => {
-            const taskKey = Object.entries(results.val()).find(
-                task => task[1].message == message
-            )[0];
-            return firebase.database().ref('chat/' + taskKey).update({
-                state: state
-            });
-        }
-    ).then(() => {
-        return firebase.database().ref('chat').once("value").then(
-            (results) => {
-                let taskToReturn = Object.entries(results.val()).find(
-                    task => task[1].message == message
-                )[1];
-                return taskToReturn;
-            }
-        )
-    });
-}
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,85 +1,21 @@
-const assert = require('chai').assert;
-global.window = global;
+const assert = require("assert"); 
+const obtenerListaMensajes = require('../src/js/test');
+let mensajes = {};
 
-const firebasemock = require('firebase-mock');
-const mockauth = new firebasemock.MockFirebase();
-const mockdatabase = new firebasemock.MockFirebase();
-mockdatabase.autoFlush();
-mockauth.autoFlush();
+describe('obtenerListaMensajes', ()=>{
+    it('debería indicar el tipo de mensaje', ()=>{
+        assert.equal(typeof mensajes, "object");
+    });
+    
+    it('debería devolver 0 si es que no hay mensajes', ()=>{
+        assert.equal(obtenerListaMensajes === mensajes.length, 0);
+    });
 
-global.firebase = firebasemock.MockFirebaseSdk(
-    path => path ? mockdatabase.child(path) : mockdatabase,
-    () => mockauth
-);
+    it('debería devolver la cantidad de mensajes obtenidos', ()=>{
+        assert.equal(obtenerListaMensajes === mensajes.length, 0);
+    });
+    it('debería  indicar si se creó una lista de mensajes', ()=>{
+        assert.equal(obtenerListaMensajes === mensajes.value, 0);
+    });
 
-require("../src/js/test");
-
-describe(
-    "Modelo de la lista de mensajes",// se empieza por lo general
-    () => {
-        describe(
-            "La lista, me debería permitir agregar mensajes",
-            () => {
-                it("Debería agregar un mensaje",
-                    (done) => {
-                        addTask("mandar un holi").then(
-                            (task) => {
-                                return getTaskList();
-                            }
-                        ).then(
-                            (taskList) => {
-                                const sendMsn = Object.entries(taskList.val())
-                                    .find(
-                                        task => {
-                                            return task[1].message == "mandar un holi";
-                                        }
-                                    );
-                                assert.exists(sendMsn[1]);//verifica que exista algo en particular en el código una función
-                                assert.equal(sendMsn[1].title, "mandar un holi");
-                                done();
-                            }
-                        ).catch(
-                            (error) => {
-                                done(error);
-                            }
-                        )
-                    }
-                );
-            }
-        );
-
-        describe(
-            "La lista, me debería permitir ver los mensajes de mis contactos",
-            () => {
-                it("Debería permitir escribir a un contacto",
-                    (done) => { //parametros de la función
-                        taskProgress("mandar un holi", "se ha mandado un holi").then(
-                            (task) => {
-                                assert.exists(task);
-                                assert.equal(task.message, "mandar un holi");
-                                assert.equal(task.state, "se ha mandado un holi");
-                                done();
-                            }
-                        ).catch(
-                            (error) => {
-                                done(error);
-                            }
-                        );
-                    })
-            }
-        );
-
-       /* describe(
-            "La lista, me debería permitir editar una tarea",
-            () => {
-            }
-        );
-
-        describe(
-            "La lista, me debería permitir borrar una tarea",
-            () => {
-
-            }
-        );*/
-    }
-);
+});
