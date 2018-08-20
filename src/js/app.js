8561e973f66672640a6da0b64b7a625139d9d187
antonella <antonella_caroca@hotmail.com>
2018-08-20 00:07:26 -0300

    escribir y leer mensajes guardados en BD

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,17 @@ window.onload = ()=>{
     });
   }
 
+  const firestore = firebase.firestore();
+  const settings = {/* your settings... */ 
+   timestampsInSnapshots: true};
+ firestore.settings(settings);
+
+/*  const timestamp = snapshot.get('created_at');
+  const date = timestamp.toDate();*/
+
+ //  Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
 let message = document.getElementById('inputMessage');
 let displayMessage = document.getElementById('col4');
 
@@ -23,53 +34,38 @@ message.addEventListener('keypress', function (e) {
     const key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
       // code for enter
-      displayMessage.innerHTML = message.value;
-      message.value='';
-    }
+      //displayMessage.innerHTML = message.value;
+      //message.value='';
+    
+    message = document.getElementById('inputMessage').value;
+
+    db.collection("users").add({
+      message: message
+    })
+    .then(function(docRef) {
+      //console.log("Document written with ID: ", docRef.id);
+      document.getElementById("inputMessage").value="";
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+       $('#myModal').modal();
+             
+             if (error.code) {
+              msn.innerHTML = error.message;
+             }
+    });
+  }
 });
 
-const firestore = firebase.firestore();
- const settings = {/* your settings... */ 
-   timestampsInSnapshots: true};
- firestore.settings(settings);
+//leer info mensajes
 
-/*  const timestamp = snapshot.get('created_at');
- const date = timestamp.toDate();*/
-
- // Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
-
-function guardarTarjeta(){
-
- message.value;
-
- db.collection("chat").add({
-   displayName: displayName,
-   message: message
- })
- .then(function(docRef) {
-   //console.log("Document written with ID: ", docRef.id);
-   document.getElementById("message").value="";
- })
- .catch(function(error) {
-   console.error("Error adding document: ", error);
-   
- });
-
-}
-
-//leer info tarjetas
- 
 db.collection("users").onSnapshot((querySnapshot) => { //se reemplaza get x onSnapshot para obtener actualizaciones en tiempo real. Tambien se saca .then
-displayMessage.innerHTML="";
- querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data().userName}`);
-     console.log(`${doc.id} => ${doc.data().message}`);
-     displayMessage.innerHTML += `
-     <div>
-         <p>${doc.data().message}</p>
-         <p>${doc.data().userName}</p>
-     </div>
-     `
- });
+  displayMessage.innerHTML="";
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().message}`);
+      displayMessage.innerHTML += `
+          <p>${doc.data().message}</p>
+      `
+  });
 });
+
